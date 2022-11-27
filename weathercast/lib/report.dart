b23334ac2e78c5ea03419d60961644f28fd0d31c
malisa33 <65111405@dpu.ac.th>
2022-11-27 13:49:14 +0700

    Day3

--- weathercast/lib/report.dart
+++ weathercast/lib/report.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:weathercast/forecast.dart';
 import 'package:weathercast/weather.dart';
 
-
 class Report extends StatefulWidget {
   const Report({super.key});
 
@@ -11,17 +10,13 @@ class Report extends StatefulWidget {
 }
 
 class _ReportState extends State<Report> {
+  Weather? _weather;
   @override
   void initState() {
-    forecast()
-    .then
-    ((weather) =>  print('${weather.condition}, ${weather.symbol}')
-    )
-    .catchError(
-      (error) => print(error)
-    );
-    super.initState();
+    updateReport();
+   super.initState();
   }
+  
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -36,21 +31,66 @@ class _ReportState extends State<Report> {
           ),          
         ),
         Container(
-          constraints: const BoxConstraints.tightFor(
+          constraints: _weather == null ?const BoxConstraints.tightFor(
             width: 150, 
             height: 150
-        ),
+        ) : null,
         decoration: BoxDecoration(
             color: Colors.blueAccent.shade700.withOpacity(0.7),
             borderRadius: BorderRadius.circular(10)
         ),
         margin: const EdgeInsets.symmetric(vertical: 30),
+        padding: const EdgeInsets.all(20),
+        child: _weather == null ? null : Column(
+          children: [
+             Text(
+              _weather!.address,
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodyText1,
+             ),
+              const SizedBox( height: 20,),
+             Text(
+              '${_weather!.temperature}℃',
+               style: Theme.of(context).textTheme.headline1,
+             ),
+              const SizedBox( height: 20,),
+             Text(
+              _weather!.condition,
+              style: Theme.of(context).textTheme.caption,
+             ), 
+              const SizedBox( height: 20,),
+               Text(
+               _weather!.symbol,
+                style: const TextStyle(
+                  fontSize: 72
+                ),
+             ),  
+          ]
+        ),
         ),
         ElevatedButton(
-        onPressed: () {}, 
+        onPressed: () {
+          updateReport();
+        }, 
         child: const Text('Refresh')
         )
       ],
     );
   }
+  void updateReport() {
+ forecast().then((weather) {
+   setState(() {
+  _weather = weather;
+  });
+   ScaffoldMessenger.of(context).removeCurrentSnackBar();
+ })
+ .catchError((error) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: error,
+      duration: const Duration(days: 1),
+      )
+  );
+ });
+ }
 }
